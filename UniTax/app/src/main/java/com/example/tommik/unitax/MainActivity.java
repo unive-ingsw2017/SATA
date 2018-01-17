f1e4abc70bdecf5e6839e6de1ba0ec060fde0abd
Unknown <834470@stud.unive.it>
2018-01-17 17:30:32 +0100

    Proto-schermata iniziale
    dopo 4 secondi compare il tutorial

--- UniTax/app/src/main/java/com/example/tommik/unitax/MainActivity.java
+++ UniTax/app/src/main/java/com/example/tommik/unitax/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.tommik.unitax;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int TIME_OUT = 4000;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, TutorialActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }

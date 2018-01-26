259573e2a4a75aa4b8b3cfbe043aa6e2da8e8142
Unknown <busatto.alessandro96@gmail.com>
2018-01-26 10:24:05 +0100

    Create activity necessaria (blank per ora)

---
+++ UniTax/app/src/main/java/com/example/tommik/unitax/InfoUni.java
@@ -0,0 +1,13 @@
+package com.example.tommik.unitax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoUni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_uni);
+    }
+}

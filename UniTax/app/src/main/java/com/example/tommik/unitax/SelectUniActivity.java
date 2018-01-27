36df0a1cea6ac104775975ebaa56ad07bc6bd926
Unknown <busatto.alessandro96@gmail.com>
2018-01-27 11:11:12 +0100

    Lo spinner fa cose e non crasha

--- UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
+++ UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
@@ -1,58 +1,17 @@
 package com.example.tommik.unitax;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    String[] uni={"Ca' Foscari"};
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Getting the instance of Spinner and applying OnItemSelectedListener on it
-        Spinner spin = (Spinner) findViewById(R.id.spinner);
-        spin.setOnItemSelectedListener(this);
-
-        //Creating the ArrayAdapter instance having the bank name list
-        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_spinner_item,uni);
-        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(ad);
-    }
-
-
-    //Performing action onItemSelected and onNothing selected
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
-        Toast.makeText(getApplicationContext(), uni[position], Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-// TODO Auto-generated method stub
-
-    }
-}
-
-/*public class SelectUniActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_uni);
-    }
-}*/
-
-/*public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
-
-   private Spinner spinner1;
+    private Spinner spinner;
     private Button btnSubmit;
 
     @Override
@@ -60,41 +19,53 @@ public class SelectUniActivity extends AppCompatActivity implements AdapterView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_uni);
 
-        //Creo lo spinner
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // When we select this spinner item, a Toast message will be displayed
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
 
-        // Spinner cliccabile
-        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-
-        // Spinner Drop down elements
-        List<String> uni = new ArrayList<String>();
-        uni.add("Ca' Foscari");
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, uni);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.uni, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        addListenerOnButton();
     }
 
 
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        btnSubmit = (Button) findViewById(R.id.buttonSelectUni);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(SelectUniActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : " +
+                                String.valueOf(spinner.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-    public void onNothingSelected(AdapterView<?> arg0) {
-        //T O D O Auto-generated method stub
     }
-
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(SelectUniActivity.this,
+                "OnClickListener : " +
+                        "\nSpinner 1 : " +
+                        String.valueOf(spinner.getSelectedItem()),
+                Toast.LENGTH_SHORT).show();
+    }
 
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //TODO Auto-generated method stub
     }
 }
-*/

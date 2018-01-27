d21d7636a51e02cf5b629122015c887dd45ee8c0
Unknown <busatto.alessandro96@gmail.com>
2018-01-27 11:49:16 +0100

    Commentato spinner cambiato in listView, da modioficare il colore

--- UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
+++ UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
@@ -1,15 +1,69 @@
 package com.example.tommik.unitax;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SelectUniActivity extends Activity {
+
+    ListView listView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_uni);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Ca Foscari",
+                "UniPD"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_2, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+    }
+
+}
+
+/*public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinner;
     private Button btnSubmit;
@@ -57,15 +111,18 @@ public class SelectUniActivity extends AppCompatActivity implements AdapterView.
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(SelectUniActivity.this,
-                "OnClickListener : " +
-                        "\nSpinner 1 : " +
-                        String.valueOf(spinner.getSelectedItem()),
-                Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(SelectUniActivity.this,
+                    "OnClickListener : " +
+                            "\nSpinner 1 : " +
+                            String.valueOf(spinner.getSelectedItem()),
+                    Toast.LENGTH_SHORT).show();
+
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        //TODO Auto-generated method stub
+        //T O D O Auto-generated method stub
     }
-}
+}*/

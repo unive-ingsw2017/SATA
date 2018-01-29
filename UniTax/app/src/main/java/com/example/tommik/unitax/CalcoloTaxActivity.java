cbc2e033d5e3657714b2565a59f4741ba568c7a4
Unknown <busatto.alessandro96@gmail.com>
2018-01-29 15:07:32 +0100

    E ce la faaaaaaaa

--- UniTax/app/src/main/java/com/example/tommik/unitax/CalcoloTaxActivity.java
+++ UniTax/app/src/main/java/com/example/tommik/unitax/CalcoloTaxActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +25,14 @@ public class CalcoloTaxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcolo_tax);
+
+        final TextView tv_merito=(TextView)findViewById(R.id.tv_merito);
+
+        final CheckBox merito=(CheckBox) findViewById(R.id.checkMerito);
+
+        final EditText isee=(EditText)findViewById(R.id.edit_ISEE);
+
+
 
         //dichiaro gli spinner
         final Spinner tipoSpinner = (Spinner) findViewById(R.id.tipologiacorso);
@@ -40,7 +51,7 @@ public class CalcoloTaxActivity extends AppCompatActivity {
         final ArrayAdapter<String> annoAdapter=new ArrayAdapter<String>(this, R.layout.spinner_item,anno);;
 
 
-        ArrayAdapter<String> cittAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, cittadinanza);
+        final ArrayAdapter<String> cittAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, cittadinanza);
 
         tipo.add("Laurea");
         tipo.add("Laurea - Studente Part-Time");
@@ -72,14 +83,15 @@ public class CalcoloTaxActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //int item =tipoSpinner.getSelectedItemPosition();
-                if(position==0||position==2){
+                if(position==0){
                     anno.clear();
                     anno.add("1");
                     anno.add("2");
                     anno.add("3");
-                    anno.add("4 e oltre");
+                    anno.add("4");
+                    anno.add("oltre 4");
                 }
-                else{
+                if(position==1){
                     anno.clear();
                     anno.add("1");
                     anno.add("2");
@@ -87,6 +99,20 @@ public class CalcoloTaxActivity extends AppCompatActivity {
                     anno.add("4");
                     anno.add("5");
                     anno.add("6");
+                }
+                if(position==2){
+                    anno.clear();
+                    anno.add("1");
+                    anno.add("2");
+                    anno.add("3");
+                    anno.add("oltre 3");
+                }
+                if(position==3){
+                    anno.clear();
+                    anno.add("1");
+                    anno.add("2");
+                    anno.add("3");
+                    anno.add("4");
                 }
                 annoAdapter.notifyDataSetChanged();
 
@@ -98,6 +124,131 @@ public class CalcoloTaxActivity extends AppCompatActivity {
                 //Another interface callback
             }
         });
+        annoSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              int ctipo=tipoSpinner.getSelectedItemPosition();
+                int canno=annoSpinner.getSelectedItemPosition();
+                int ccit=position;
+
+                if(ctipo==0 && canno==0 || ctipo==1 && canno==0){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("voto esame di stato da 98 a 100/100 oppure da 58 a 60/60");
+                }
+                if(ctipo==0 && canno==1){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 54 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                // LAUREA
+                if(ctipo==0 && canno==2){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 108 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                if(ctipo==0 && canno>=3){
+                    tv_merito.setVisibility(View.INVISIBLE);
+                    merito.setVisibility(View.INVISIBLE);
+                }
+
+                //LAUREA PART - TIME
+                if(ctipo==1 && canno==1){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 27 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                if(ctipo==1 && canno==2){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 54 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+
+                if(ctipo==1 && canno==3){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 81 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+
+                if(ctipo==1 && canno==4){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 108 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                if(ctipo==1 && canno>=5){
+                    tv_merito.setVisibility(View.INVISIBLE);
+                    merito.setVisibility(View.INVISIBLE);
+                }
+
+                // MAGISTRALE
+                if(ctipo==2 && canno==0 || ctipo==3 && canno==0){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("ha un voto di laurea che consente l'accesso di almeno 108/110 per i corsi di Laurea magistrale di area economica e scientifica, 110/110 per i corsi di Laurea magistrale di area linguistica e umanistica");
+                }
+
+                if(ctipo==2 && canno==1){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 54 crediti entro il 30/09/2017 con una media ponderata di almeno: 29/30 per i corsi di laurea di area economica, 30/30 per i corsi di laurea di area linguistica, scientifica o umanistica" );
+                }
+                if(ctipo==2 && canno>=2){
+                    tv_merito.setVisibility(View.INVISIBLE);
+                    merito.setVisibility(View.INVISIBLE);
+                }
+                //PART-TIME
+                if(ctipo==3 && canno==1){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 27 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                if(ctipo==1 && canno==2){
+                    tv_merito.setVisibility(View.VISIBLE);
+                    merito.setVisibility(View.VISIBLE);
+
+                    merito.setText("conseguiti almeno 54 crediti entro il 30/09/2017 con una media ponderata di almeno: 28/30 per i corsi di laurea di area economica, 29/30 per i corsi di laurea di area linguistica o scientifica, 30/30 per i corsi di laurea di area umanistica" );
+                }
+                if(ctipo==3 && canno>=3){
+                    tv_merito.setVisibility(View.INVISIBLE);
+                    merito.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
+        });
+
+        cittSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 2){
+                    isee.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    isee.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
+        });
+
 
 
 
--- UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
+++ UniTax/app/src/main/java/com/example/tommik/unitax/SelectUniActivity.java
@@ -10,7 +10,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SelectUniActivity extends Activity {
 
@@ -78,8 +77,7 @@ public class SelectUniActivity extends Activity {
                 itemValue    = (String) listView.getItemAtPosition(position);
 
                 // Show Alert
-                Toast.makeText(getApplicationContext(),
-                        "Hai selezionato: " +itemValue , Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), "Hai selezionato: " +itemValue , Toast.LENGTH_LONG).show();
 
             }
 
@@ -94,67 +92,3 @@ public class SelectUniActivity extends Activity {
     }
 
 }
-
-/*public class SelectUniActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private Spinner spinner;
-    private Button btnSubmit;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_uni);
-
-        // When we select this spinner item, a Toast message will be displayed
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.uni, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        addListenerOnButton();
-    }
-
-
-    // get the selected dropdown list value
-    public void addListenerOnButton() {
-
-        spinner = (Spinner) findViewById(R.id.spinner);
-        btnSubmit = (Button) findViewById(R.id.buttonSelectUni);
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(SelectUniActivity.this,
-                        "OnClickListener : " +
-                                "\nSpinner 1 : " +
-                                String.valueOf(spinner.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            Toast.makeText(SelectUniActivity.this,
-                    "OnClickListener : " +
-                            "\nSpinner 1 : " +
-                            String.valueOf(spinner.getSelectedItem()),
-                    Toast.LENGTH_SHORT).show();
-
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        //T O D O Auto-generated method stub
-    }
-}*/
